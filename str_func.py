e53255eb5c5375d8583f51beaa8941d901e63e45
Vladislav Murzin <autotropa@yandex.ru>
2023-03-09 21:46:05 +0300

    add docstrings and func stroke_title

--- str_func.py
+++ str_func.py
@@ -1,4 +1,15 @@
 def stroke ():
+    '''
+    Выводит строку заглавными буквами
+    :return:
+    '''
     line = input("Введите строку:")
     return line.upper()
 
+def stroke_title ():
+    '''
+    выводит каждое слово с заглавной буквы
+    :return:
+    '''
+    line = input("Введите строку:")
+    return line.title()
